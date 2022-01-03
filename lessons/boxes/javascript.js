3402d5b97b932b736a3e4bbfc207a262b37fce79
Bri <bhilliker@gmail.com>
2022-01-03 09:02:16 -0600

    deactivate buttons once completed

--- lessons/boxes/javascript.js
+++ lessons/boxes/javascript.js
@@ -1,19 +1,20 @@
 document.getElementById('button1').onclick = function() {grow()};
 function grow() {
     document.getElementById('button1').innerHTML = `It's Bigger!`;
-
+    document.getElementById('button1').disabled = true;
+    document.getElementById('button2').disabled = false;
     document.getElementById('box').style = 'height:200px; width:200px; background-color:orange; margin:25px'
 };
 
 document.getElementById('button2').onclick = function() {blue()};
 function blue() {
     document.getElementById('box').style.backgroundColor = 'blue';
-
+    document.getElementById('button2').disabled = true;
 }
 
 document.getElementById('button3').onclick = function() {fade()};
 function fade() {
     document.getElementById('box').style.opacity = '50%';
     document.getElementById('box').style.transition = '2s';
-
+    document.getElementById('button3').disabled = true;
 } 
