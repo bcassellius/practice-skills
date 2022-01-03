6029a60cbcf843700b20fc5a0946744750e73c9a
Bri <bhilliker@gmail.com>
2022-01-03 08:47:51 -0600

    add color change to blue

--- lessons/boxes/javascript.js
+++ lessons/boxes/javascript.js
@@ -2,5 +2,9 @@ document.getElementById('button1').onclick = function() {grow()};
 function grow() {
     document.getElementById('button1').innerHTML = `It's Bigger!`;
     document.getElementById('box').style = 'height:200px; width:200px; background-color:orange; margin:25px'
+};
+
+document.getElementById('button2').onclick = function() {blue()};
+function blue() {
+    document.getElementById('box').style.backgroundColor = 'blue';
 }
-;
