71de41a94f5e5a7410857b24dd17a1f9c7affb32
Bri <bhilliker@gmail.com>
2022-01-03 09:01:24 -0600

    add fade transition button functionality

--- lessons/boxes/javascript.js
+++ lessons/boxes/javascript.js
@@ -1,10 +1,19 @@
 document.getElementById('button1').onclick = function() {grow()};
 function grow() {
     document.getElementById('button1').innerHTML = `It's Bigger!`;
+
     document.getElementById('box').style = 'height:200px; width:200px; background-color:orange; margin:25px'
 };
 
 document.getElementById('button2').onclick = function() {blue()};
 function blue() {
     document.getElementById('box').style.backgroundColor = 'blue';
+
 }
+
+document.getElementById('button3').onclick = function() {fade()};
+function fade() {
+    document.getElementById('box').style.opacity = '50%';
+    document.getElementById('box').style.transition = '2s';
+
+} 
