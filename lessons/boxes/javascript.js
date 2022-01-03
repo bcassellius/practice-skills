7f4e63d76120a17f575bb58c83eddd388d1d7e87
Bri <bhilliker@gmail.com>
2022-01-03 09:11:27 -0600

    add reset button functionality

--- lessons/boxes/javascript.js
+++ lessons/boxes/javascript.js
@@ -3,6 +3,7 @@ function grow() {
     document.getElementById('button1').innerHTML = `It's Bigger!`;
     document.getElementById('button1').disabled = true;
     document.getElementById('button2').disabled = false;
+    document.getElementById('button3').disabled = false;
     document.getElementById('box').style = 'height:200px; width:200px; background-color:orange; margin:25px'
 };
 
@@ -17,4 +18,13 @@ function fade() {
     document.getElementById('box').style.opacity = '50%';
     document.getElementById('box').style.transition = '2s';
     document.getElementById('button3').disabled = true;
-} 
+}
+
+document.getElementById('button4').onclick = function() {reset()};
+function reset() {
+    document.getElementById('box').style = 'height:150px; width:150px; background-color:orange; margin:25px';
+    document.getElementById('button1').innerHTML = `Grow`;
+    document.getElementById('button1').disabled = false;
+    document.getElementById('button2').disabled = false;
+    document.getElementById('button3').disabled = false;
+}
